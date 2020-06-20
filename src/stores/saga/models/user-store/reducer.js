008ef3bb209798b8a1589d72fbd0371a8bfe412d
fatmaRubaie <fatma1211994@hotmail.com>
2020-06-20 13:08:32 +0300

    d

--- src/stores/saga/models/user-store/reducer.js
+++ src/stores/saga/models/user-store/reducer.js
@@ -12,13 +12,11 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-    console.log('reducer action',action)
     switch (action.type) {
         case clear: return {...initialState, loading: 'clear'};
         case login: return {loading: 'login'};
         case success: return {...action['form'], loading: 'success',...initialState};
-        case error: return {password_error: action['form'].password[0],
-            email_error: action['form'].email, non_field_errors:action['form'].non_field_errors[0],loading: 'error', ...initialState}
+        case error: return {non_field_errors:action['form'].non_field_errors[0], loading: 'error', ...initialState}
         default:
             return state;
     }
--- src/stores/saga/models/user-store/sagas.js
+++ src/stores/saga/models/user-store/sagas.js
@@ -1,25 +1,28 @@
 import {takeLatest, put, takeEvery} from 'redux-saga/effects';
 
 import {login, success, error} from './actions';
-import {user_login} from "../../../../services/auth";
+import {user_login} from '../../../../services/auth';
 
 const handler = function* () {
     yield takeLatest(login, loginApi);
-}
+};
 
 
 function* loginApi(action) {
+    console.log('api',action)
+    let result = '';
     try {
-        let result = yield user_login(action.form)
-        console.log('api',result)
+        result = yield user_login(action.form);
         if (result['token']) {
-            yield put({type: success, form: result})
-        } else {
-            yield put({type: error, form: result})
+            yield put({type: success, form: result});
         }
-    } catch (err) {
-        console.log('err', err)
+        else {
+            yield put({type: error, form: result});
+        }
 
+    } catch (err) {
+        yield put({type: error, form: result});
+        console.log('saga', result);
     }
 }
 
