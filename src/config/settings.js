2807720cf7ec4958d37da86aa5cb365d2998d78b
Mohammed Lubbad <mlub@outlook.com>
2020-07-15 18:10:56 +0300

    change Api url to new server

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
 export const settings = {
     //API_URL: 'http://sha.smartpace.co.uk/api/v1/'
-     API_URL: 'https://sha.smartpace.co.uk/api/v1/',// 'http://95.111.251.63:8000/api/v1/'
+     API_URL: 'https://drr.smartpace.co.uk/api/v1/',// 'http://95.111.251.63:8000/api/v1/'
 };
 
 export default settings;
